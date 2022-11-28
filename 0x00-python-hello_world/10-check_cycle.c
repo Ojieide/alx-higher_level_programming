b6d1581e81ede5f61f7686dd23620bea34d73dd1
Ojieide Egbomondion <ojieide@yahoo.com>
2022-11-28 23:27:50 +0000

    Linked list cycle header file

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,36 +1,25 @@
-/*
- * File: 10-check_cycle.c
- * Auth: Brennan D Baraban
- */
-
+#include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
 
 /**
- * check_cycle - Checks if a singly-linked list contains a cycle.
- * @list: A singly-linked list.
- *
- * Return: If there is no cycle - 0.
- *         If there is a cycle - 1.
+ * check_cycle - checks if a singly linked list has a cycle in it
+ * @list: input head
+ * Return: 0 if there is no cycle, 1 if there is a cycle
  */
+
 int check_cycle(listint_t *list)
 {
-	listint_t *turtle, *hare;
+	listint_t *tmp;
 
-	if (list == NULL || list->next == NULL)
+	if (!list)
 		return (0);
-
-	turtle = list->next;
-	hare = list->next->next;
-
-	while (turtle && hare && hare->next)
+	while (list)
 	{
-		if (turtle == hare)
+		tmp = list;
+		list = list->next;
+		if (tmp <= list)
 			return (1);
-
-		turtle = turtle->next;
-		hare = hare->next->next;
 	}
-
 	return (0);
 }
