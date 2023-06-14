4355af57948553c2aae9a6d280a133ad9a75ee34
Ojieide Egbomondion <ojieide@yahoo.com>
2023-06-14 20:38:46 +0000

    JavaScript - Objects, Scopes and Closures

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -3,6 +3,7 @@ module.exports = class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
   }
+
   print () {
     for (let y = 0; y < this.height; y++) console.log('X'.repeat(this.width));
   }
