674634e6e6dd4b377e9050ec994a1478f0d5d902
Ojieide Egbomondion <ojieide@yahoo.com>
2023-06-14 02:04:50 +0000

    JavaScript - Warm up

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-const size = Math.floor(Number(process.argv[2]));
-if (isNaN(size)) {
+const arg1 = Math.floor(Number(process.argv[2]));
+if (isNaN(arg1)) {
   console.log('Missing size');
 } else {
-  for (let r = 0; r < size; r++) {
-    let row = '';
-    for (let c = 0; c < size; c++) row += 'X';
-    console.log(row);
+  for (let i = 0; i < arg1; i++) {
+    let ch = '';
+    for (let j = 0; j < arg1; j++) ch += 'X';
+    console.log(ch);
   }
 }
