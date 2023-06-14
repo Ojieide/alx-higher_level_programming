5006f5dd6427cc8afa343971981f21b9147702d2
Ojieide Egbomondion <ojieide@yahoo.com>
2023-06-14 01:19:17 +0000

    JavaScript - Warm up

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
-const count = process.argv.length;
-console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');
+const args = process.argv.length;
+console.log(args === 2 ? 'No argument' : args === 3 ? 'Argument found' : 'Arguments found');
