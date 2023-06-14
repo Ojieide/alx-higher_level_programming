3427fec07d738e90498e95f442c8d874a9457216
Ojieide Egbomondion <ojieide@yahoo.com>
2023-06-14 20:48:10 +0000

    JavaScript - Objects, Scopes and Closures

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
-const dict = require('./101-data.js').dict;
-let newDict = {};
-for (let key in dict) {
-  if (newDict[dict[key]] === undefined) {
-    newDict[dict[key]] = [key];
+const dict1 = require('./101-data.js').dict;
+const dict2 = {};
+for (const key in dict1) {
+  const value = dict1[key];
+  if (value in dict2) {
+    dict2[value].push(key);
   } else {
-    newDict[dict[key]].push(key);
+    dict2[value] = [key];
   }
 }
-console.log(newDict);
+console.log(dict2);
