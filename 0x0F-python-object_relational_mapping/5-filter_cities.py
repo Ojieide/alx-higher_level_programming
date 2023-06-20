f284595b67559086935ca0df3fc2f06b71726a00
Ojieide Egbomondion <ojieide@yahoo.com>
2023-06-20 04:23:09 +0000

    All cities by state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -10,9 +10,9 @@ import MySQLdb
 
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM `cities` as `c` \
+    cu = db.cursor()
+    cu.execute("SELECT * FROM `cities` as `c` \
                 INNER JOIN `states` as `s` \
                    ON `c`.`state_id` = `s`.`id` \
                 ORDER BY `c`.`id`")
-    print(", ".join([city[2] for city in cursor.fetchall() if city[4] == sys.argv[4]]))
+    print(", ".join([ct[2] for ct in cu.fetchall() if ct[4] == sys.argv[4]]))
