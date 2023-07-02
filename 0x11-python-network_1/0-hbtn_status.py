921320e2590800452a0ec4e0d5a81df47e672f71
Ojieide Egbomondion <ojieide@yahoo.com>
2023-07-02 20:25:47 +0000

    Whats my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-""" Python script that fetches https://alx-intranet.hbtn.io/status """
-import urllib.request
+"""
+Python script that fetches https://alx-intranet.hbtn.io/status
+"""
 
 
 if __name__ == "__main__":
