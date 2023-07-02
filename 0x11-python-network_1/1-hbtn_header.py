120426f279b422f4efe7eb51925a870c5dcd3353
Ojieide Egbomondion <ojieide@yahoo.com>
2023-07-02 20:32:45 +0000

    Response header value #0

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Displays the value of the X-Request-Id variable found in the header of the response
+Displays the value of the X-Request-Id variable
+found in the header of the response
 """
 import sys
 import urllib.request
